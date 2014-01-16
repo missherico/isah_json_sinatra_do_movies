b4b62640ee792f461450844f6b0edf5b435f5e28
Isa Herico <miss.herico@gmail.com>
2014-01-15 21:12:32 -0800

    REST route to picture page works, used string interpolation. Picture displays too.

--- omdb.rb
+++ omdb.rb
@@ -27,13 +27,14 @@ post '/result' do
  
   # puts @result["Search"].each {|movies_hash|
   # puts "#{movies_hash['Title']} - #{movies_hash['year']}."}
-
+  
 
   # Modify the html output so that a list of movies is provided.
   html_str = "<html><head><title>Movie Search Results</title></head><body><Table><h1>Movie Results</h1>\n<th align=right>YEAR</th><th align=left>&nbsp;&nbsp;TITLE</th>"
-  
-  result["Search"].each {|movies_hash|
-    html_str += "<tr><td align=right>#{movies_hash['Year']}&nbsp;&nbsp;</td> <td>&nbsp;&nbsp;#{movies_hash['Title']} </td><td>#{movies_hash['imdbID']}</tr>"
+   #sort HASH before iteration!!!!!!
+
+  result["Search"].map {|movies_hash|
+    html_str += "<tr><td align=right>#{movies_hash['Year']}&nbsp;&nbsp;</a></td><td><a href='/poster/#{movies_hash['imdbID']}'>&nbsp;&nbsp;#{movies_hash['Title']} </td></tr>"
   }
 
   html_str += "</table></body></html>"
@@ -41,9 +42,22 @@ post '/result' do
 end
 
 get '/poster/:imdb' do |imdb_id|
+  imdb_id = params[:imdb]
+
   # Make another api call here to get the url of the poster.
-  html_str = "<html><head><title>Movie Poster</title></head><body><h1>Movie Poster</h1>\n"
-  html_str = "<h3>#{imdb_id}</h3>"
+
+  response = Typhoeus.get("http://www.omdbapi.com/", :params => {:i => imdb_id})
+  result = JSON.parse(response.body)
+
+  img = result['Poster']
+
+
+  html_str = "<html><head><title>Movie Poster</title></head><body><h1>Movie Poster:</h1>\n"
+  
+  html_str += "<table><tr><h3>#{result['Title']}</h3></tr>"
+
+  html_str += "<tr><img src=#{img} /></tr></table>"
+
   html_str += '<br /><a href="/">New Search</a></body></html>'
 
 end
